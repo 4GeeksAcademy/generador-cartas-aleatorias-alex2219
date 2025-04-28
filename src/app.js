9266d9418fb86122ac4dbb7a4581a187e1e3e4c0
Fredy Alexander Camelo <88693018+alex2219@users.noreply.github.com>
2025-04-28 04:43:36 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let symbolSelect = symbol[Math.floor(Math.random()*symbol.length)];
+  let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let valueRandom = value[Math.floor(Math.random()*value.length)];
+  
+  document.querySelector("#symbol-top").innerHTML = symbolSelect
+  document.querySelector("#value").innerHTML = valueRandom
+  document.querySelector("#symbol-bottom").innerHTML = symbolSelect
+  console.log(symbol);
 };
